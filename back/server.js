dbc1ce154aaa984b7a4c5960508d51f016596122
RojoDev <rojo.valisoa.dev@gmail.com>
2022-03-24 22:56:52 +0300

    [IMP]:Mongodb's initialisation and connection to database

--- back/server.js
+++ back/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:admin@e-kaly.p3zus.mongodb.net/ekaly?retryWrites=true&w=majority";
 
 app.listen(3000, function() {
     console.log('listening on 3000')
@@ -8,3 +10,11 @@ app.listen(3000, function() {
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log('Connected to Database')
+  client.close();
+});
